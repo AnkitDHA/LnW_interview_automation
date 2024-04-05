426b468834cb7f697ad2bdaadd342607a03ddb38
AnkitDHA <ankitd75@gmail.com>
2024-04-05 14:22:35 +0530

    Headless mode congid

--- src/test/java/Utils/baseClass.java
+++ src/test/java/Utils/baseClass.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -21,8 +22,10 @@ public class baseClass {
 
 	public static WebDriver initDriver() {
 		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
 		
-		driver = new ChromeDriver();
+		driver = new ChromeDriver(options);
 		wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 		driver.get("https://www.amazon.in/");
 	    
